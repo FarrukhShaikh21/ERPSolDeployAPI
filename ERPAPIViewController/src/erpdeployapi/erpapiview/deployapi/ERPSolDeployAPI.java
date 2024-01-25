9a4104a60dd55695720acf422442d72847f93d47
Farrukh Shaikh <mefarrukh@hotmail.com>
2024-01-26 00:03:51 +0500

    customer information

--- ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
+++ ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
@@ -1,5 +1,18 @@
 package erpdeployapi.erpapiview.deployapi;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import oracle.adf.share.ADFContext;
+
+import oracle.jbo.ApplicationModule;
+import oracle.jbo.Row;
+import oracle.jbo.ViewObject;
+import oracle.jbo.client.Configuration;
+
+@Path("deployapi")
 public class ERPSolDeployAPI {
     public ERPSolDeployAPI() {
         super();
@@ -8,9 +21,67 @@ public class ERPSolDeployAPI {
     private final String DEPLOYAPIACCESSKEY = "tH$@cCessK$Y";
 
 
-        public String getDeployCustomer(String pApiAuthKey) {
-                 String result = "{\n \"ReturnCode\":\"00\",\"CustomerInfoResult\":[" + "\"Customername\":\"NoCustomer" + " \n]\n}";
-                   return result;
+    @GET
+    @Produces("application/json")
+    @Path("/getDeployCustomer")
+    public String getDeployCustomer(@QueryParam("pApiAuthKey") String pApiAuthKey) {
+                   System.out.println("hello");
+                             ADFContext oldContext = ADFContext.initADFContext(null, null, null, null);
+                             String countryinfo = null;
+                             try {
+                                 // System.out.println("this is GhsStGenCustomerVORO");
+                                 String amDef = "erpdeployapi.erpapimodel.erpapiam.ERPAPIAppModule";
+                                 String config = "ERPAPIAppModuleLocal";
+                                 ApplicationModule am = Configuration.createRootApplicationModule(amDef, config);
+                                 ViewObject vo = am.findViewObject("VwCustomerApiRO");
+
+                                 while (vo.hasNext()) {
+                                     Row r = vo.next();
+                                     if (countryinfo == null) {
+                                         countryinfo =
+                                             "{\"CustomerName\":\"" + r.getAttribute("CustomerName") + 
+                                             "\",\"Customerid\":\"" + r.getAttribute("Customerid") +
+                                             "\",\"AccountOpeningDate\":\"" + r.getAttribute("AcOpeningDate") + "\"" +
+                                             ",\"Location\":\"" + r.getAttribute("Locationid") + "\"" +
+                                             ",\"Address\":\"" +r.getAttribute("Address") + 
+                                         ",\"SalespersonName\":\"" +r.getAttribute("Name") + 
+                                         ",\"TerritoryName\":\"" +r.getAttribute("TerritoryName") + 
+                                         ",\"SalesterritoryId\":\"" +r.getAttribute("Salesterritoryid") + 
+                                         ",\"SalespersonId\":\"" +r.getAttribute("Salespersonid") +     
+                                         ",\"DivisionId\":\"" +r.getAttribute("Divid") +     
+                                         ",\"DivisionName\":\"" +r.getAttribute("DivisionName") + 
+                                         ",\"DefaultPerson\":\"" +r.getAttribute("DefaultPerson") + 
+                                          ",\"CnicNo\":\"" + r.getAttribute("CnicNo") +
+                                             "\"}";
+                                         ////  System.out.println(countryinfo);
+                                     } else {
+                                         countryinfo +=
+                                         "\n , {\"CustomerName\":\"" + r.getAttribute("CustomerName") + 
+                                         "\",\"Customerid\":\"" + r.getAttribute("Customerid") +
+                                         "\",\"AccountOpeningDate\":\"" + r.getAttribute("AcOpeningDate") + "\"" +
+                                         ",\"Location\":\"" + r.getAttribute("Locationid") + "\"" +
+                                         ",\"Address\":\"" +r.getAttribute("Address") + 
+                                         ",\"SalespersonName\":\"" +r.getAttribute("Name") +
+                                         ",\"TerritoryName\":\"" +r.getAttribute("TerritoryName") +
+                                         ",\"SalesterritoryId\":\"" +r.getAttribute("Salesterritoryid") +
+                                         ",\"SalespersonId\":\"" +r.getAttribute("Salespersonid") +
+                                         ",\"DivisionId\":\"" +r.getAttribute("Divid") +
+                                         ",\"DivisionName\":\"" +r.getAttribute("DivisionName") +
+                                         ",\"DefaultPerson\":\"" +r.getAttribute("DefaultPerson") +
+                                         ",\"CnicNo\":\"" + r.getAttribute("CnicNo") +
+                                         "\"}";
+                                         
+                                     }
+                                 }
+                                 // Work with your appmodule and view object here
+                                 Configuration.releaseRootApplicationModule(am, true);
+                                 System.out.println("this is wscalling-end");
+                             } finally {
+                                 ADFContext.resetADFContext(oldContext);
+                             }
+
+                             String result = "{\n \"ReturnCode\":\"00\",\"CustomerInfoResult\":[" + countryinfo + " \n]\n}";
+                             return result;
                }
         
     }
