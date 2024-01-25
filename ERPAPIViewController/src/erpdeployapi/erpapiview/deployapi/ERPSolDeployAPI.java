78584e5268a7eccd343cff6d16b07a062ed265bc
Farrukh Shaikh <mefarrukh@hotmail.com>
2024-01-25 23:19:51 +0500

    class adding

--- ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
+++ ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
@@ -4,4 +4,14 @@ public class ERPSolDeployAPI {
     public ERPSolDeployAPI() {
         super();
     }
-}
+    
+    private final String DEPLOYAPIACCESSKEY = "tH$@cCessK$Y";
+
+
+        public String getDeployCustomer(String pApiAuthKey) {
+                 String result = "{\n \"ReturnCode\":\"00\",\"CustomerInfoResult\":[" + "\"Customername\":\"NoCustomer" + " \n]\n}";
+                   return result;
+               }
+        
+    }
+
