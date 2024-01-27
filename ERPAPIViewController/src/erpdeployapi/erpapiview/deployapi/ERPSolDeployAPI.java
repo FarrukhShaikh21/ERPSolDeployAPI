920c48622fd53871f5559f25a73af207a0637593
Farrukh Shaikh <mefarrukh@hotmail.com>
2024-01-27 09:32:42 +0500

    adding return and product apis

--- ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
+++ ERPAPIViewController/src/erpdeployapi/erpapiview/deployapi/ERPSolDeployAPI.java
@@ -158,7 +158,131 @@ public class ERPSolDeployAPI {
                                  String result = "{\n \"ReturnCode\":\"00\",\"SaleInfoResult\":[" + countryinfo + " \n]\n}";
                                  return result;
                    }
-    
-        
+
+
+    @GET
+    @Produces("application/json")
+    @Path("/getDeployProduct")
+    public String getDeployProduct(@QueryParam("pApiAuthKey") String pApiAuthKey) {
+                       System.out.println("hello");
+                       if (pApiAuthKey==null || !(pApiAuthKey.equals(DEPLOYAPIACCESSKEY))) {
+               return "{\n \"ReturnCode\":\"00\",\"ProductInfoResult\":[" + "\"Not Authorized\"" + "]\n}";
+           }
+                                 ADFContext oldContext = ADFContext.initADFContext(null, null, null, null);
+                                 String countryinfo = null;
+                                 try {
+                                     // System.out.println("this is GhsStGenCustomerVORO");
+                                     String amDef = "erpdeployapi.erpapimodel.erpapiam.ERPAPIAppModule";
+                                     String config = "ERPAPIAppModuleLocal";
+                                     ApplicationModule am = Configuration.createRootApplicationModule(amDef, config);
+                                     ViewObject vo = am.findViewObject("VwProductApiRO");
+
+                                     while (vo.hasNext()) {
+                                         Row r = vo.next();
+                                         if (countryinfo == null) {
+                                             countryinfo =
+                                                 "{\"Productid\":\"" + r.getAttribute("Productid") + 
+                                                 "\",\"ModelNo\":\"" + r.getAttribute("ModelNo") +
+                                                 "\",\"Groupid\":\"" + r.getAttribute("Groupid") + "\"" +
+                                                 ",\"GroupName\":\"" + r.getAttribute("GroupName") + "\"" +
+                                                 ",\"Divid\":\"" +r.getAttribute("Divid") + "\"" +
+                                             ",\"DivName\":\"" +r.getAttribute("DivName") +
+                                                 "\"}";
+                                             ////  System.out.println(countryinfo);
+                                         } else {
+                                             countryinfo +=
+                                             "\n ,{\"Productid\":\"" + r.getAttribute("Productid") + 
+                                             "\",\"ModelNo\":\"" + r.getAttribute("ModelNo") +
+                                             "\",\"Groupid\":\"" + r.getAttribute("Groupid") + "\"" +
+                                             ",\"GroupName\":\"" + r.getAttribute("GroupName") + "\"" +
+                                             ",\"Divid\":\"" +r.getAttribute("Divid") + "\"" +
+                                             ",\"DivName\":\"" +r.getAttribute("DivName") +
+                                             "\"}";
+                                             
+                                         }
+                                     }
+                                     // Work with your appmodule and view object here
+                                     Configuration.releaseRootApplicationModule(am, true);
+                                     System.out.println("this is wscalling-end");
+                                 } finally {
+                                     ADFContext.resetADFContext(oldContext);
+                                 }
+
+                                 String result = "{\n \"ReturnCode\":\"00\",\"ProductInfoResult\":[" + countryinfo + " \n]\n}";
+                                 return result;
+                   }
+
+    @GET
+    @Produces("application/json")
+    @Path("/getDeploySaleReturn")
+    public String getDeploySaleReturn(@QueryParam("pApiAuthKey") String pApiAuthKey,
+                                      @QueryParam("pStartDate") String pStartDate,
+                                      @QueryParam("pEndDate") String pEndDate) {
+                           if (pApiAuthKey==null || !(pApiAuthKey.equals(DEPLOYAPIACCESSKEY))) {
+                               return "{\n \"ReturnCode\":\"00\",\"SaleReturnInfoResult\":[" + "\"Not Authorized\"" + "]\n}";
+                           }
+                           System.out.println("hello");
+                                     ADFContext oldContext = ADFContext.initADFContext(null, null, null, null);
+                                     String countryinfo = null;
+                                     try {
+                                         // System.out.println("this is GhsStGenCustomerVORO");
+                                         String amDef = "erpdeployapi.erpapimodel.erpapiam.ERPAPIAppModule";
+                                         String config = "ERPAPIAppModuleLocal";
+                                         ApplicationModule am = Configuration.createRootApplicationModule(amDef, config);
+                                         ViewObject vo = am.findViewObject("VwSaleReturnApiRO");
+                                         vo.setWhereClause("SALE_RET_DATE between TO_DATE('"+pStartDate+"','yyyy-mm-dd') AND TO_DATE('"+pEndDate+"','yyyy-mm-dd') AND TO_DATE('"+pEndDate+"','yyyy-mm-dd') - TO_DATE('"+pStartDate+"','yyyy-mm-dd')<=1");
+                                         System.out.println(vo.getWhereClause());
+                                         vo.executeQuery();
+                                         vo.setRangeSize(-1);
+                                         while (vo.hasNext()) {
+                                             Row r = vo.next();
+                                             if (countryinfo == null) {
+                                                 countryinfo =
+                                                    "{\"Salesretid\":\"" + r.getAttribute("Salesretid") + "\"" +
+                                                    ",\"SaleRetDate\":\"" + r.getAttribute("SaleRetDate") +"\"" +
+                                                    ",\"Storeid\":\"" + r.getAttribute("Storeid") + "\"" +
+                                                    ",\"StoreName\":\"" + r.getAttribute("StoreName") + "\"" +
+                                                    ",\"DealerId\":\"" +r.getAttribute("DealerId") + "\"" + 
+                                                    ",\"DealerName\":\"" +r.getAttribute("DealerName") + "\"" + 
+                                                    ",\"Salespersonid\":\"" +r.getAttribute("Salespersonid") + "\"" + 
+                                                    ",\"SalesPerson\":\"" +r.getAttribute("SalesPerson") + "\"" + 
+                                                    ",\"Location\":\"" +r.getAttribute("Location") + "\"" +     
+                                                    ",\"Productid\":\"" +r.getAttribute("Productid") + "\"" +     
+                                                    ",\"ModelNo\":\"" +r.getAttribute("ModelNo") + "\"" + 
+                                                    ",\"Quantity\":" +r.getAttribute("Quantity")  + 
+                                                    ",\"ImeiNo\":\"" + r.getAttribute("ImeiNo") + "\"}";
+                                                 ////  System.out.println(countryinfo);
+                                             } else {
+                                                 countryinfo +=
+                                                 "\n , {\"Salesretid\":\"" + r.getAttribute("Salesretid") + "\"" +
+                                                 ",\"SaleRetDate\":\"" + r.getAttribute("SaleRetDate") +"\"" +
+                                                 ",\"Storeid\":\"" + r.getAttribute("Storeid") + "\"" +
+                                                 ",\"StoreName\":\"" + r.getAttribute("StoreName") + "\"" +
+                                                 ",\"DealerId\":\"" +r.getAttribute("DealerId") + "\"" + 
+                                                 ",\"DealerName\":\"" +r.getAttribute("DealerName") + "\"" + 
+                                                 ",\"Salespersonid\":\"" +r.getAttribute("Salespersonid") + "\"" + 
+                                                 ",\"SalesPerson\":\"" +r.getAttribute("SalesPerson") + "\"" + 
+                                                 ",\"Location\":\"" +r.getAttribute("Location") + "\"" +     
+                                                 ",\"Productid\":\"" +r.getAttribute("Productid") + "\"" +     
+                                                 ",\"ModelNo\":\"" +r.getAttribute("ModelNo") + "\"" + 
+                                                 ",\"Quantity\":" +r.getAttribute("Quantity")  + 
+                                                 ",\"ImeiNo\":\"" + r.getAttribute("ImeiNo")+"\"}";
+                                                 
+                                             }
+                                             
+                                         }
+                                         // Work with your appmodule and view object here
+                                         Configuration.releaseRootApplicationModule(am, true);
+                                         System.out.println("this is wscalling-end");
+                                     } 
+                                     catch(Exception ex){
+                                         return "{\n \"ReturnCode\":\"00\",\"SaleReturnInfoResult\":[" + "\"InvalidDateParameters\"" + "]\n}";
+                                     } finally {
+                                         ADFContext.resetADFContext(oldContext);
+                                     }
+
+                                     String result = "{\n \"ReturnCode\":\"00\",\"SaleReturnInfoResult\":[" + countryinfo + " \n]\n}";
+                                     return result;
+                       } 
     }
 
